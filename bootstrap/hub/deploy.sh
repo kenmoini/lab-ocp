20e8891f223be8232409ba87cc38b53ae53df30b
Ken Moini <ken@kenmoini.com>
2025-11-16 02:18:17 -0500

    exec and wave reorder

--- bootstrap/hub/deploy.sh
+++ bootstrap/hub/deploy.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+set -e
+
 # This script deploys ArgoCD to a Hub OpenShift cluster.  It will:
 # - Install the OpenShift GitOps Operator
 # - Wait for the ArgoCD Application Controller to be ready
 # - [Optional Step not included] Create a Secret for Git credentials if needed
 # - Apply the ArgoCD Application that self-configures ArgoCD and pulls in Hub GitOps configuration
 
-oc apply -k https://raw.githubusercontent.com/kenmoini/ocp4-gitops-config/refs/heads/main/openshift/openshift-gitops/operator/overlays/latest/kustomization.yaml
+oc create secret generic vault-userpass -n kube-system --from-literal=username=openshift --from-file=password=$HOME/.vault_pass.txt --dry-run=client -o yaml | oc apply -f -
+
+oc apply -k https://github.com/kenmoini/ocp4-gitops-config/openshift/openshift-gitops/operator/overlays/latest/
 
 # Wait for ArgoCD to be ready
 echo "Waiting for ArgoCD to be ready..."
-until oc wait --for=condition=Available=True --timeout=10s deployment/openshift-gitops-application-controller -n openshift-gitops; do
+until oc wait --for=condition=Available=True --timeout=10s statefulset/openshift-gitops-application-controller -n openshift-gitops; do
   echo "ArgoCD is not ready yet. Retrying in 10 seconds..."
   sleep 10
 done
